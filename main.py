f19500cea375aa5685a6abf6e8a0f4e6be67dab7
Qlbbl1111 <quinnlburke@icloud.com>
2022-03-16 00:02:14 -0700

    Started work on fetching gif from API.

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ import time
 from bot_token import token
 
 author_id = '892999941146963969'
-api_key= ''
-img = 'https://i.imgur.com/kPLPAJs.jpg'
 activity = discord.Game(name="with your feelings")
 channel_id = 923432498925547531
+
+url = 'https://qlbbl-api.net'
+
 
 #Command prefix.
 bot = commands.Bot(
@@ -29,6 +30,20 @@ async def on_ready():  # When the bot is ready
     print(bot.user)
  
 
+#Gif
+@bot.command(name='gif', help='Gets a random gif from the qlbbl-api.')
+async def gif(ctx):
+
+    response = requests.get(f"{url}/api/v1/hazbin/gif/random")
+    r = json.loads(response.text)
+
+    await ctx.send(r)
+
+@gif.error
+async def gif_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('Error: Something went wrong.')
+
 
 #Test
 @bot.command(name='test', help='Tests the bot.')
@@ -44,10 +59,11 @@ async def test_error(ctx, error):
 
 
 #Message 1
+"""
 @tasks.loop(seconds=86400)
 async def called_once_a_day():
   message_channel = bot.get_channel(channel_id)
-  await message_channel.send("__Is the new episode of Helluva Boss out yet?__")
+  await message_channel.send("__Is part 2 of the Helluva Boss finale out yet?__")
   time.sleep(3)
   await message_channel.send(img)
 
@@ -55,12 +71,12 @@ async def called_once_a_day():
 async def before():
     await bot.wait_until_ready()
     print("Finished waiting")
-
+"""
 
 if __name__ == '__main__':
   pass
 
 
-called_once_a_day.start()
+#called_once_a_day.start()
 
 bot.run(token)  # Starts the bot
